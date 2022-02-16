1fa62942b53ba38e32eda5da7f745af72bd9c01a
psykovski-extended <dominik.lovetinsky99@gmail.com>
2022-02-16 10:45:06 +0100

    initial commit

---
+++ app/src/main/java/android/io/binobo/connector/Configuration.java
@@ -0,0 +1,35 @@
+package android.io.binobo.connector;
+
+public class Configuration {
+
+    public enum State {
+        UNKNOWN_STATE,
+        CONFIG_START,
+        WIFI_DATA_VALID,
+        WIFI_DATA_INVALID,
+        TOKEN_VALID,
+        TOKEN_INVALID,
+        LOCAL_DATA_FOUND,
+        WIFI_CONFIG,
+        TOKEN,
+        CALIBRATION,
+        CALIBRATION_ZERO_POS,
+        CALIBRATION_DONE,
+        WEB_SOCKET_CONNECTING,
+        WEB_SOCKET_CONNECTED,
+        STORE_DATA,
+        DATA_STORED,
+        DONE
+    }
+
+    public static State getState (String state) {
+        switch (state) {
+            case "[ESP32]: Configuration starts...": return State.CONFIG_START;
+            case "Use local stored config data? [y/n]: ": return State.LOCAL_DATA_FOUND;
+            case "SSID:": return State.WIFI_CONFIG;
+            case "Token:": return State.TOKEN;
+            default: return State.UNKNOWN_STATE;
+        }
+    }
+
+}
---
+++ app/src/main/java/android/io/binobo/connector/Globals.java
@@ -0,0 +1,20 @@
+package android.io.binobo.connector;
+
+import android.hardware.usb.UsbDeviceConnection;
+
+import com.hoho.android.usbserial.driver.UsbSerialDriver;
+import com.hoho.android.usbserial.driver.UsbSerialPort;
+import com.hoho.android.usbserial.util.SerialInputOutputManager;
+
+import java.util.Vector;
+
+public class Globals {
+
+    public static UsbSerialDriver usbSerialDriver;
+    public static UsbDeviceConnection connection;
+    public static UsbSerialPort port;
+    public static SerialInputOutputManager serialIOManager;
+    public static final Vector<String> uartData = new Vector<>();
+    public static Configuration.State configState = Configuration.State.UNKNOWN_STATE;
+
+}
